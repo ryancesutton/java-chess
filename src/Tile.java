ef014d48aca608a4e806960d1c0938fca2119f5a
Ry Sutton <ryancesutton@gmail.com>
2021-02-05 11:18:49 -0600

    add occupied and getPiece method

--- src/Tile.java
+++ src/Tile.java
@@ -1,4 +1,12 @@
-public class Tile {
+public abstract class Tile {
 
+    int tileCoordinate;
 
+    public Tile(int tileCoordinate) {
+        this.tileCoordinate = tileCoordinate;
+    }
+
+    public abstract boolean isTileOccupied();
+
+    public abstract Piece getPiece();
 }
