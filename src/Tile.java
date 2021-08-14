33929d1921ed188b95b79b8cfae27542c473d315
Ry Sutton <ryancesutton@gmail.com>
2021-08-14 16:27:32 -0500

    complete initial tile class and subclasses

--- src/Tile.java
+++ src/Tile.java
@@ -9,4 +9,43 @@ public abstract class Tile {
     public abstract boolean isTileOccupied();
 
     public abstract Piece getPiece();
+
+    public static final class EmptyTile extends Tile {
+        public EmptyTile(int coordinates) {
+            super(coordinates);
+        }
+
+        @Override
+        public boolean isTileOccupied() {
+            return false;
+        }
+
+        @Override
+        public Piece getPiece() {
+            return null;
+        }
+
+    }
+
+    public static final class OccupiedTile extends Tile {
+
+        Piece pieceOnTile;
+
+        public OccupiedTile(int tileCoordinate, Piece pieceOnTile) {
+            super(tileCoordinate);
+            this.pieceOnTile = pieceOnTile;
+        }
+
+        @Override
+        public boolean isTileOccupied() {
+            return true;
+        }
+
+        @Override
+        public Piece getPiece() {
+            return this.pieceOnTile;
+        }
+
+
+    }
 }
